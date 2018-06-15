fee887203d8045eab207af9269818b9b2bcfd56c
Paul Choi <q1431708@mac1254912.qvcdev.qvc.net>
2018-06-15 15:53:50 -0400

    init

--- PlayingCard/ViewController.swift
+++ PlayingCard/ViewController.swift
@@ -12,14 +12,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var deck = PlayingCardDeck()
+        
+        for _ in 1...10 {
+            if let card = deck.draw() {
+                print("\(card)")
+            }
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
